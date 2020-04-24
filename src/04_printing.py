edb05423f7707d70bb03345b5c927eec1babb4f1
Jacqueline Omollo <you@example.com>
2020-04-23 23:36:23 -0400

    Done with printing section

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,17 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print(x, y, z)
 
 # Use the 'format' string method to print the same thing
+name = input("What's your name? ")
+
+if name == "Jackie":
+    print("Hello, nice to see you {}".format(name))
+
+print("Have a nice day!")
 
 # Finally, print the same thing using an f-string
+name = "Eden"
+age = 42
+print(f"Hello, Her name is {name} and she is {age} years old.")

4861cb8c2b81339c4a1722550afd4c924094be9b
Jacqueline Omollo <you@example.com>
2020-05-04 14:41:44 -0400

    Done with list

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,6 +1,8 @@
 # For the exercise, look up the methods and functions that are available for use
 # with Python lists.
 
+import numpy
+
 x = [1, 2, 3]
 y = [8, 9, 10]
 
@@ -8,22 +10,39 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+for i in range(8, 11):
+    x.append(
+        i
+    )
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)
 print(x)
-
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+for t in range(len(x)):
+    print(x[t])
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+# def multiply(theList):
+#     results = 1000
+#     for x in theList:
+#         results = results * x
+#     return results
+#     print(multiply(x * 1000))
+
+values = numpy.prod(x)
+print(values * 1000)

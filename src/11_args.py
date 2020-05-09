f748b2411d996c77e5b230d9796955bd4d1f06d5
Jacqueline Omollo <you@example.com>
2020-05-08 20:35:54 -0400

    11 is done

--- src/11_args.py
+++ src/11_args.py
@@ -6,8 +6,11 @@
 
 # YOUR CODE HERE
 def f1(x: int, y: int):
-    return x +  y
+    return x + y
+
+
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum.
@@ -15,16 +18,19 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-  return sum(args)
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
+    return sum(args)
+
+
+print(f2(1))  # Should print 1
+print(f2(1, 3))  # Should print 4
+print(f2(1, 4, -12))  # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(*a))    # Should print 22
+print(f2(*a))  # Should print 22
+
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -32,10 +38,11 @@ print(f2(*a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-def f3( x: int, y:int)
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+def f3(n, s= 1):
+    return n + s
 
+print(f3(1, 2))  # Should print 3
+print(f3(8))  # Should print 9
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
@@ -46,7 +53,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**banana):
+    for key, value in banana.items():
+        print("key: %s,  value: %s" %(key, value))
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -64,4 +73,10 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
+
+def f5(a, b, *args, **kwargs):
+    print(a, b)
+    print(args)
+    print(kwargs)
+
